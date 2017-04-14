c589a3eeafdc7864ac971a25c37c06fc46fc8c3e
Grigz <grigswa@gmail.com>
2017-04-14 11:13:44 +0200

    updated prod settings

--- MySite/settings/production.py
+++ MySite/settings/production.py
@@ -147,7 +147,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.normpath(os.path.join(PROJECT_ROOT, 'staticfiles'))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
