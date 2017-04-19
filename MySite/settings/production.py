b2f99a25c8c2794c304bf8f965d8d6bdcefecadc
Grigz <grigswa@gmail.com>
2017-04-19 12:32:34 +0200

    modified styling for login page, navbar, and naming convention for home and base directories.

--- MySite/settings/production.py
+++ MySite/settings/production.py
@@ -158,7 +158,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT2, 'static'),
+    os.path.join(PROJECT_ROOT2, "static"),
 )
 
 # Simplified static file serving.
--- home/views.py
+++ home/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'home/Home.html')
+    return render(request, 'base/Home.html')
