8b53c24be4185b9cb5d58aecdc972bc6f1aeb610
griggz <grigswa@gmail.com>
2017-05-09 16:59:41 +0200

    Cont. to build on posts app and designed basic style for look and feel.

--- posts/urls.py
+++ posts/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     url(r'^$', views.posts_list, name='list'),
     url(r'^create/$', views.posts_create, name='create'),
     url(r'^detail/(?P<id>[0-9]+)/$', views.posts_detail, name='detail'),
-    url(r'^update/$', views.posts_update, name='update'),
-    url(r'^delete/$', views.posts_delete, name='delete'),
+    url(r'^(?P<id>[0-9]+)/edit/$', views.posts_update, name='update'),
+    url(r'^(?P<id>[0-9]+)/delete/$', views.posts_delete, name='delete'),
 
 ]
+
+
--- posts/views.py
+++ posts/views.py
@@ -1,22 +1,45 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Post
-
-
+from .forms import PostForm
+from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
+
 def posts_list(request):
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 3)  # Show 25 posts per page
+    page_request_var = "page"
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
     context = {
             "object_list": queryset,
-            "title": "List"
+            "title": "List",
+            "page_request_var": page_request_var,
         }
 
-    return render(request, 'posts/index.html', context)
+    return render(request, 'posts/post-list.html', context)
 
 
 def posts_create(request):
-    return HttpResponse("<h1>HELLO</h1>")
+    form = PostForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Your Post has been created!")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "form": form,
+    }
+    return render(request, "posts/post_form.html", context)
 
 
 def posts_detail(request, id=None):
@@ -28,9 +51,24 @@ def posts_detail(request, id=None):
     return render(request, 'posts/detail.html', context)
 
 
-def posts_update(request):
-    return HttpResponse("<h1>Update</h1>")
+def posts_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Your changes have been saved!")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, 'posts/post_form.html', context)
 
 
-def posts_delete(request):
-    return HttpResponse("<h1>Delete</h1>")
+def posts_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Post Successfully Deleted")
+    return redirect("posts:list")
