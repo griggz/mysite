91fec3598d64e2d9f93484f106cf7c874290cbdb
griggz <grigswa@gmail.com>
2017-05-18 16:03:10 +0200

    corrected width x height mixup

--- posts/forms.py
+++ posts/forms.py
@@ -8,7 +8,7 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         widgets = {'unsplash_url': HiddenInput()}
-        fields = ['user', 'title', 'content', 'post_image', 'height_field', 'width_field', 'unsplash_url', 'draft', 'publish', 'read_time']
+        fields = ['user', 'title', 'content', 'post_image', 'width_field', 'height_field', 'unsplash_url', 'draft', 'publish', 'read_time']
         # exclude = ('field1',)
         labels = {'user': 'Author', 'title': 'Title', 'slug': 'Slug', 'content': 'Content', 'post_image': 'Post Image', 'height_field': 'Image Height', 'width_field': 'Image Width', 'draft': "Draft", 'publish': 'Publish Date'}
         help_texts = {'post_image': 'Get your photo ID from <a href="https://unsplash.com" target="_blank">Unsplash.com</a>. Use this <a href="http://quick.as/x3vycpgog" target="_blank">guide</a> if you need help. Or, if you want a random image, copy/paste this url "https://source.unsplash.com/random" to pull random images from unsplash.',
@@ -25,7 +25,7 @@ class PostForm(ModelForm):
         else:
             unsplash_api = str("https://source.unsplash.com")
             unsplash_image_id = str(post_image)
-            image_size = str(image_height), str(image_width)
+            image_size = str(image_width), str(image_height)
             join_size = 'x'.join(image_size)
             join_fields = unsplash_api, unsplash_image_id, join_size
             compile_url = '/'.join(join_fields)
