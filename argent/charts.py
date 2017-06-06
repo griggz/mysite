5c882ca14f032f8d80d9abb94f4d504ddc7254fe
griggz <grigswa@gmail.com>
2017-06-06 12:44:28 +0200

    developing progress bar; bug fix on adding new items

--- argent/charts.py
+++ argent/charts.py
@@ -19,6 +19,12 @@ class ChartData(APIView):
     permission_classes = []
 
     def get(self, request, format=None):
+        savobject = Savings.objects.get(id=2)
+
+        # Total Savings/Spent
+        qs_spending = savobject.total_spent_dollars
+        qs_savings = savobject.total_savings_display
+
         # SAVINGS
         # qs_November16 = Entry.objects.filter(date__range=('2016-11-1', '2016-11-30')).aggregate(s=Sum('dollars_sum')).get('s')
         qs_December16 = Entry.objects.filter(date__range=('2016-12-1', '2016-12-31')).aggregate(s=Sum('dollars_sum')).get('s')
@@ -40,10 +46,14 @@ class ChartData(APIView):
         qs2_June17 = Entry.objects.filter(date__range=('2017-6-1', '2017-6-30')).aggregate(s=Sum('daily_savings_display')).get('s')
 
         labels = ["DEC16", "JAN17", "FEB17", "MAR17", "APR17", "MAY17", "JUNE17"]
+        labels2 = ["Spent", "Saved"]
+        totals = [qs_spending, qs_savings]
         spending = [qs_December16, qs_January17, qs_February17, qs_March17, qs_April17, qs_May17, qs_June17]
         savings = [qs2_December16, qs2_January17, qs2_February17, qs2_March17, qs2_April17, qs2_May17, qs2_June17]
         data = {
             "labels": labels,
+            "labels2": labels2,
+            "totals": totals,
             "spending": spending,
             "savings": savings
         }
@@ -56,31 +66,17 @@ class DashData(APIView):  # FOR TESTING WITH argent/dash and argent/api/data
     permission_classes = []
 
     def get(self, request, format=None):
-        # SAVINGS
-        qs_November16 = Entry.objects.filter(date__range=('2016-11-1', '2016-11-30')).aggregate(s=Sum('dollars_sum')).get('s')
-        qs_December16 = Entry.objects.filter(date__range=('2016-12-1', '2016-12-31')).aggregate(s=Sum('dollars_sum')).get('s')
-        qs_January17 = Entry.objects.filter(date__range=('2017-1-1', '2017-1-31')).aggregate(s=Sum('dollars_sum')).get('s')
-        qs_February17 = Entry.objects.filter(date__range=('2017-2-1', '2017-2-28')).aggregate(s=Sum('dollars_sum')).get('s')
-        qs_March17 = Entry.objects.filter(date__range=('2017-3-1', '2017-3-31')).aggregate(s=Sum('dollars_sum')).get('s')
-        qs_April17 = Entry.objects.filter(date__range=('2017-4-1', '2017-4-30')).aggregate(s=Sum('dollars_sum')).get('s')
-        qs_May17 = Entry.objects.filter(date__range=('2017-5-1', '2017-5-31')).aggregate(s=Sum('dollars_sum')).get('s')
+        savobject = Savings.objects.get(id=2)
 
-        # SPENDING
-        qs2_November16 = Entry.objects.filter(date__range=('2016-11-1', '2016-11-30')).aggregate(s=Sum('daily_savings_display')).get('s')
-        qs2_December16 = Entry.objects.filter(date__range=('2016-12-1', '2016-12-31')).aggregate(s=Sum('daily_savings_display')).get('s')
-        qs2_January17 = Entry.objects.filter(date__range=('2017-1-1', '2017-1-31')).aggregate(s=Sum('daily_savings_display')).get('s')
-        qs2_February17 = Entry.objects.filter(date__range=('2017-2-1', '2017-2-28')).aggregate(s=Sum('daily_savings_display')).get('s')
-        qs2_March17 = Entry.objects.filter(date__range=('2017-3-1', '2017-3-31')).aggregate(s=Sum('daily_savings_display')).get('s')
-        qs2_April17 = Entry.objects.filter(date__range=('2017-4-1', '2017-4-30')).aggregate(s=Sum('daily_savings_display')).get('s')
-        qs2_May17 = Entry.objects.filter(date__range=('2017-5-1', '2017-5-31')).aggregate(s=Sum('daily_savings_display')).get('s')
+        # Total Savings/Spent
+        qs_spending = savobject.total_savings_display
+        qs_savings = savobject.total_spent_dollars
 
-        labels = ["NOV16", "DEC16", "JAN17", "FEB17", "MAR17", "APR17", "MAY17"]
-        spending = [qs_November16, qs_December16, qs_January17, qs_February17, qs_March17, qs_April17, qs_May17]
-        savings = [qs2_November16, qs2_December16, qs2_January17, qs2_February17, qs2_March17, qs2_April17, qs2_May17]
+        labels = ["Total Spent", "Total Saved",]
+        totals = [qs_spending, qs_savings]
         data = {
             "labels": labels,
-            "spending": spending,
-            "savings": savings
+            "totals": totals,
         }
 
         return Response(data)
