f51428dbd6ffbb03dfd0952772213ebc18e79ddb
Wayne <grigswa@gmail.com>
2017-11-27 15:31:38 -0500

    Reverted to Whitenoise from using AWS S3 to host static files due to potential costs asociate.

--- MySite/settings/production.py
+++ MySite/settings/production.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 import dj_database_url
-import datetime
-from MySite.settings.sensitive import AWS_KEY_ID, AWS_ACCESS_KEY
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -66,7 +64,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django_hosts.middleware.HostsRequestMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -165,40 +163,15 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-# STATIC_URL = '/static/'
-#
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static"),
-# )
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
-#
-# MEDIA_URL = "/media/"
-#
-# MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "media-root")
+STATIC_URL = '/static/'
 
-AWS_ACCESS_KEY_ID = AWS_KEY_ID
-AWS_SECRET_ACCESS_KEY = AWS_ACCESS_KEY
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
-AWS_FILE_EXPIRE = 200
-AWS_PRELOAD_METADATA = True
-AWS_QUERYSTRING_AUTH = True
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
 
-DEFAULT_FILE_STORAGE = 'MySite.utils.MediaRootS3BotoStorage'
-STATICFILES_STORAGE = 'MySite.utils.StaticRootS3BotoStorage'
-AWS_STORAGE_BUCKET_NAME = 'vvayneco'
-S3DIRECT_REGION = 'us-east'
-S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_ROOT = MEDIA_URL
-STATIC_URL = S3_URL + 'static/'
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+MEDIA_URL = "/media/"
 
-two_months = datetime.timedelta(days=61)
-date_two_months_later = datetime.date.today() + two_months
-expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "media-root")
 
-AWS_HEADERS = {
-    'Expires': expires,
-    'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()),),
-}
