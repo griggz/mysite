d20ff7ad3dc23a6115eeca12394663e058e8b4b3
Wayne <grigswa@gmail.com>
2019-08-28 13:47:44 -0400

    updating env to include staging pipe

--- MySite/settings/production.py
+++ MySite/settings/production.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'check host location'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.vvayne.io', 'vvayne.io', 'wgsite.herokuapp.com', '0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['www.vvayne.io', 'vvayne.io', 'wgsite.herokuapp.com', 'serene-fjord-89115.herokuapp.com']
 
 EMAIL_HOST = 'mail.name.com'
 EMAIL_HOST_USER = 'wayne@vvayne.io'
